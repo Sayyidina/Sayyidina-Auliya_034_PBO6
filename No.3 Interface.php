79b670a0769b7724e571f2e43d39ae9d925405b4
Sayyidina Auliya <100737140+Sayyidina@users.noreply.github.com>
2022-10-20 23:51:54 +0700

    Add files via upload

---
+++ No.3 Interface.php
@@ -0,0 +1,14 @@
+<!-- Sayyidina Auliya_21091397034 -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
