eac8299224442543e34212669978115c181814e5
Sayyidina Auliya <100737140+Sayyidina@users.noreply.github.com>
2022-10-20 23:52:18 +0700

    Add files via upload

---
+++ No.4 Interface.php
@@ -0,0 +1,16 @@
+<!--- Nama    : Sayyidina Auliya
+      NIM     : 21091397034
+      Kelas   : 2021 b--->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
